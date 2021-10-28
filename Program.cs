d61fa6bc14b04d596e8f380d41bbfe1b7e646c23
shinpei yoshihara <s-yoshihara@saitama-tec.co.jp>
2021-10-28 23:03:55 +0900

    dotnet proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tiny_blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
