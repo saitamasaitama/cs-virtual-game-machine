a8b20fc1a4127b4910380f648f1c3f3a99c4669d
shinpei yoshihara <s-yoshihara@saitama-tec.co.jp>
2021-10-28 23:47:55 +0900

    work

--- Unity/BlockChain.cs
+++ Unity/BlockChain.cs
@@ -1,46 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 /**
  * this is not Unity script
  */
-public class BlockChain<GAMEDATA>
-    where GAMEDATA : SnapShot
+public class BlockChain<G>
+    where G : SnapShot<G>
 {
-    public Block Current;
+    public Block<G> Current;
 
+    //永続ストレージからの読み込み
+    public void sync(){
+    }
 }
 
-public abstract class Block
+public struct BlockChainMeta{
+}
+
+public abstract class Block<T>
+  where T : SnapShot<T>
 {
     public BlockMeta meta;
-
     public BlockChain<T> BlockChain;
-
-    public abstract Diff BlockDiff();
-    public abstract SnapShot CalcSnapShot();
-    
-    
-    
+    public abstract Diff<T> BlockDiff();
+    public abstract SnapShot<T> CalcSnapShot();
+    public abstract Block<T> AddTransaction(Transaction<T> t);
 }
 
 public struct BlockMeta
 {
     public readonly int blockNumber;
     public readonly bool isUpdated;
-
-
 }
 
-public class Diff
+public class Diff<T>
+  where T : SnapShot<T>
 {
-    public List<Transaction> transactions;
-    
+    public List<Transaction<T>> transactions;   
 }
 
-public class Transaction
+public class Transaction<T>
+  where T : SnapShot<T>
 {
     
 }
@@ -49,14 +50,16 @@ public class Transaction
 /// <summary>
 /// シリアライズ化されたゲームデータを示す
 /// </summary>
-public class SnapShot
-{
-    
-    
-    public Diff SnapAndDiff()
+public class SnapShot<T> 
+where T : SnapShot<T>
+{   
+    public Diff<T> SnapAndDiff()
     {
         //ブロックを更新する
-        
+ 
         return null;
+    }
+    public Block<T> CreateNewBlock(){
+      return null;
     }
 }
